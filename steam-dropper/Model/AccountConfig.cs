b99c015efdcaa748195c59ed6fa667578d2cbff0
Titoncio <titoncio@gmail.com>
2021-09-08 09:46:28 -0300

    Changing the default TimeConfig values

--- steam-dropper/Model/AccountConfig.cs
+++ steam-dropper/Model/AccountConfig.cs
@@ -74,7 +74,7 @@ namespace steam_dropper.Model
 
 
             Name = Path.GetFileNameWithoutExtension(path);
-            TimeConfig = obj.TimeConfig ?? MainConfig.Config.TimeConfig ?? new TimeConfig {IdleTime = 60,  PauseBetweenIdleTime = 660} ;
+            TimeConfig = obj.TimeConfig ?? MainConfig.Config.TimeConfig ?? new TimeConfig {IdleTime = 120,  PauseBetweenIdleTime = 300} ;
             FilePath = path;
         }
 
