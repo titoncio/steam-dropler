6f382f4a23e15deb38334437476462b7677fdded
Titoncio <titoncio@gmail.com>
2021-11-16 22:12:15 -0300

    Add default values

--- steam-dropper/Model/TimeConfig.cs
+++ steam-dropper/Model/TimeConfig.cs
@@ -4,8 +4,8 @@ namespace steam_dropper.Model
 {
     public class TimeConfig
     {
-        public int IdleTime { get; set; }
+        public int IdleTime { get; set; } = 120;
 
-        public int PauseBetweenIdleTime { get; set; }
+        public int PauseBetweenIdleTime { get; set; } = 300;
     }
 }
